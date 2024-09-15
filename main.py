166a31f64b9b21ed39f5533c5499f19dc8384e14
SaadNabeel <saadnabeel2010@gmail.com>
2024-09-15 15:14:31 +0500

    initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+def fact(x):
+    if x==1 or x==0:
+        return 1
+    else: 
+        return x*fact(x-1)
+    
+n=int(input("Enter any number:"))
+if n<0:
+    print("Factorial does not exist")
+else:
+    print("Factorial is",fact(n))
